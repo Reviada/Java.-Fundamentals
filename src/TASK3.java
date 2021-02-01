1ee277c8db822456eb244a32e3cdb8c05752b4ca
Abasheva Dasha <Abashevadasha@gmail.com>
2021-02-01 13:41:33 +0400

    Task №3 random (Вывести заданное количество случайных чисел с переходом и без перехода на новую строку)

---
+++ src/TASK3.java
@@ -0,0 +1,8 @@
+public class TASK3 {
+    public static void main (String [] args) {
+        int [] num = {5,10,2,1};
+        for ( int i : num)
+            //System.out.print(i);
+            System.out.println(i);
+    }
+}
